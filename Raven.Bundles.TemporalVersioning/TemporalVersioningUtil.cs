5321ea9cf3c095f8deae32c9c804962c36e4d804
Matt Johnson <mj1856@hotmail.com>
2013-01-11 19:09:55 -0700

    Don't wait on new thread anymore
    We no longer call this code from a trigger, so we don't have to be in a
    separate thread.

--- Raven.Bundles.TemporalVersioning/TemporalVersioningUtil.cs
+++ Raven.Bundles.TemporalVersioning/TemporalVersioningUtil.cs
@@ -106,46 +106,28 @@ namespace Raven.Bundles.TemporalVersioning
 
         public static void WaitForIndexToBecomeNonStale(this DocumentDatabase database, string name, DateTime? cutOff, Guid? cutoffEtag)
         {
-            // This has to be done on a separate thread ahead of time.  We can't do it at time of query.
-            // Because we are in an trigger, the index stats do not get updated if we stay in the same thread.
-            // That leads to a condition where the index is not stale, but we still think it is.
-            // Waiting in a task on a separate thread works around this problem.
-            //
-            // Per Oren - this is by design because of esent snapshot isolation.
-            // It does work with Munin in Raven >= 2.0.2158, but having it on a separate thread is still ok.
-            //
-            // http://issues.hibernatingrhinos.com/issue/RavenDB-708
-            //
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-            var task = Task.Factory.StartNew(() =>
-                {
-                    var stopwatch = new Stopwatch();
-                    stopwatch.Start();
+            while (true)
+            {
+                if (database.Disposed)
+                    break;
 
-                    while (true)
-                    {
-                        if (database.Disposed)
-                            break;
+                var stale = true;
+                database.TransactionalStorage.Batch(x => { stale = x.Staleness.IsIndexStale(name, cutOff, cutoffEtag); });
 
-                        var stale = true;
-                        database.TransactionalStorage.Batch(x =>
-                            {
-                                stale = x.Staleness.IsIndexStale(name, cutOff, cutoffEtag);
-                            });
+                if (!stale)
+                    break;
 
-                        if (!stale)
-                            break;
+                if (stopwatch.Elapsed >= TimeSpan.FromSeconds(30))
+                    throw new TimeoutException(
+                        string.Format("Over 30 seconds have elapsed while waiting for the \"{0}\" index to catch up.", name));
 
-                        if (stopwatch.Elapsed >= TimeSpan.FromSeconds(30))
-                            throw new TimeoutException(
-                                string.Format("Over 30 seconds have elapsed while waiting for the \"{0}\" index to catch up.", name));
+                Thread.Sleep(100);
+            }
 
-                        Thread.Sleep(100);
-                    }
-
-                    stopwatch.Stop();
-                });
-            task.Wait();
+            stopwatch.Stop();
         }
     }
 }
