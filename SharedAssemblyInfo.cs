be224f2837afff762b4985fd98f0063aee121057
Matt Johnson <mj1856@hotmail.com>
2013-01-14 15:00:55 -0700

    Set version to 2.0.7.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("RavenDB Temporal Versioning Bundle")]
 [assembly: AssemblyCopyright("Copyright © 2012 Matt Johnson")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.7.2")]
-[assembly: AssemblyFileVersion("2.0.7.2")]
+[assembly: AssemblyVersion("2.0.7.3")]
+[assembly: AssemblyFileVersion("2.0.7.3")]
