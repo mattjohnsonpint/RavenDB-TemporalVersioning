9e0d3ca918cbfb15d56c66df3dee6550d835d596
Matt Johnson <mj1856@hotmail.com>
2013-07-22 17:27:36 -0700

    Support .net 4.5 under raven 2.5

---
+++ Raven.Client.Bundles.TemporalVersioning-4.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Client for RavenDB Temporal Versioning Bundle")]
+[assembly: AssemblyDescription("RavenDB 'Temporal Versioning' bundle client.  Allows for tracking of changes to entities at different effective dates.")]
