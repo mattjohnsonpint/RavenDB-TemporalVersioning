2eb2f15fb4230e9ad75fec36c67ed8b4f6e6278e
Matt Johnson <mj1856@hotmail.com>
2013-09-16 14:58:40 -0700

    Repair unit test per #6.  Now fails.

--- Raven.Bundles.Tests.TemporalVersioning/NonCurrentLoadTests.cs
+++ Raven.Bundles.Tests.TemporalVersioning/NonCurrentLoadTests.cs
@@ -51,10 +51,12 @@ namespace Raven.Bundles.Tests.TemporalVersioning
         [Fact]
         public void TemporalVersioning_NonCurrentLoad_WithFutureEdit()
         {
+            var now = DateTimeOffset.Now;
+
             using (var documentStore = this.GetTemporalDocumentStore())
             {
                 const string id = "employees/1";
-                var effectiveDate1 = new DateTimeOffset(new DateTime(2012, 1, 1));
+                var effectiveDate1 = now.AddMonths(-2);
                 using (var session = documentStore.OpenSession())
                 {
                     var employee = new Employee { Id = id, Name = "John", PayRate = 10 };
@@ -64,7 +66,7 @@ namespace Raven.Bundles.Tests.TemporalVersioning
                 }
 
                 // Make some changes
-                var effectiveDate2 = new DateTimeOffset(new DateTime(2012, 2, 1));
+                var effectiveDate2 = now.AddMonths(-1);
                 using (var session = documentStore.OpenSession())
                 {
                     var employee = session.Effective(effectiveDate2).Load<Employee>(id);
@@ -74,7 +76,7 @@ namespace Raven.Bundles.Tests.TemporalVersioning
                 }
 
                 // Make some future changes
-                var effectiveDate3 = new DateTimeOffset(new DateTime(2012, 3, 1));
+                var effectiveDate3 = now.AddMonths(1);
                 using (var session = documentStore.OpenSession())
                 {
                     var employee = session.Effective(effectiveDate3).Load<Employee>(id);
@@ -91,7 +93,7 @@ namespace Raven.Bundles.Tests.TemporalVersioning
                     Assert.Equal(30, employee.PayRate);
 
                     var temporal = session.Advanced.GetTemporalMetadataFor(employee);
-                    Assert.Equal(TemporalStatus.Current, temporal.Status);
+                    Assert.Equal(TemporalStatus.Revision, temporal.Status);
                     Assert.Equal(3, temporal.RevisionNumber);
                 }
             }
